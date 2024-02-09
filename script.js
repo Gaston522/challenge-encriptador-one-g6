9f8f6bd6438a826efc2ecede7889be62bd7a3aa0
Gaston522 <gastoncoyant@gmail.com>
2024-02-09 12:08:06 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+function encriptarDesencriptar(op) {//Funcion que encripta o desencripta dependiendo del valor de su parametro
+    let textoArea = document.querySelector(".textA").value;//Variente que toma el value del textArea
+    if (textoArea != "") {
+        if (op === "encriptar") {//Encripta usando el metodo .replaceAll para reemplazar las vocales por las frases encriptadoras
+            document.querySelector(".parrafo").innerText = textoArea.replaceAll("e", "enter").replaceAll("i", "imes").replaceAll("a", "ai").replaceAll("o", "ober").replaceAll("u", "ufat");
+        }else if(op == "desencriptar"){//Desencripta utilizando el mismo metodo pero invirtiendo el proceso
+            document.querySelector(".parrafo").innerText = textoArea.replaceAll("ai", "a").replaceAll("enter", "e").replaceAll("imes", "i").replaceAll("ober", "o").replaceAll("ufat", "u");
+        }
+        document.querySelector(".container-img").style.display = "none";//Se oculta el container-img
+        document.querySelector(".container-p").style.display = "flex";//Se muestra el container-p
+    }
+}
+
+function bCopiar() {
+    navigator.clipboard.writeText(document.querySelector(".parrafo").innerText).then(()=>{//Copia usando un metodo que devuelve una promesa
+        alert("Mensaje copiado");//Si copia muestra el mensaje y reseta los valores del textArea, container-img y container-p
+        document.querySelector(".textA").value = "";
+        document.querySelector(".container-img").style.display = "flex";
+        document.querySelector(".container-p").style.display = "none";
+        
+    }).catch(()=>alert("No se pudo copiar el mensaje"));//Si no pudo copiar, muestra el mensaje
+}
